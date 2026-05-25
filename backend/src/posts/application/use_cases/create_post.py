b0d87f2e4136713394d5a9c5dd96fb01b349d873
nurislam-td <ntasbolatov03@gmail.com>
2026-05-25 14:45:51 +0600

    fix create post handler

--- backend/src/posts/application/use_cases/create_post.py
+++ backend/src/posts/application/use_cases/create_post.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from auth.application.ports import UnitOfWork
+
 from posts.application.dto.post import CreatedPostDTO, CreatePostDTO
 from posts.application.ports import IPostService
 
@@ -7,6 +9,9 @@ from posts.application.ports import IPostService
 @dataclass(slots=True, frozen=True)
 class CreatePostHandler:
     _post_service: IPostService
+    _uow: UnitOfWork
 
     async def call(self, post: CreatePostDTO) -> CreatedPostDTO:
-        return await self._post_service.create_post(post)
+        ok = await self._post_service.create_post(post)
+        await self._uow.commit()
+        return ok
